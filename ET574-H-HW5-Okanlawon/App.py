aa1fca0f02056535fca28b82f4d0b7e0ac8e80c9
KayodeOkanlawon <kayode.okanlawon61@student.qcc.cuny.edu>
2026-05-12 02:07:36 +0000

    Refactored GUI into application class

--- ET574-H-HW5-Okanlawon/App.py
+++ ET574-H-HW5-Okanlawon/App.py
@@ -1,12 +1,19 @@
 import wx
 
+class StudentVisualizer(wx.Frame):
+
+    def __init__(self):
+
+        super().__init__(
+            parent=None,
+            title="Student Performance Visualizer",
+            size=(700, 500)
+        )
+
+        panel = wx.Panel(self)
+
+        self.Show()
+
 app = wx.App()
-
-frame = wx.Frame(
-    parent=None,
-    title="Student Performance Visualizer"
-)
-
-frame.Show()
-
+frame = StudentVisualizer()
 app.MainLoop()
